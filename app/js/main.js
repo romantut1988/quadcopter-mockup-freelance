cd664e479084875cb3b79dd2c21a7f3b9ad0f63e
romantut1988 <romantut1988@mail.ru>
2022-01-22 01:10:22 +0300

    questions vector styles

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,10 @@ $(function () {
             '</svg>\n</button>',
     });
 
+    $('questions__item-title').on('click', function() {
+
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+    });
+
 })
